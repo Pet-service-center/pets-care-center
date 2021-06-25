a5c551ec84da4f8f98f788be4f93d1aa19315866
anas-abusaif <anasabusief@gmail.com>
2021-06-25 20:35:49 +0300

    design update

--- js/petclinic.js
+++ js/petclinic.js
@@ -1,20 +1,18 @@
 /* eslint-disable no-unused-vars */
 'use strict';
+let oppontment=document.getElementById('oppontment');
 
 /////////////********function to handle the submit of the form*******/////////////
 function appointmentFormSubmission(event) {
   event.preventDefault();
   let FullName = document.getElementById('fname').value;
-  document.getElementById('appointmentForm').innerHTML = (`${Date()},
-  your form is submited , we will call you soon ${FullName}  `);
+  let submition=document.createElement('p');
+  oppontment.appendChild(submition);
+  submition.innerHTML = (`${Date()},your form is submited , we will call you soon ${FullName}`);
+  document.getElementById('appointform').reset();
 }
-
 
 let appointform = document.getElementById('appointform');
 appointform.addEventListener('submit', appointmentFormSubmission);
 
 ////////**********function to reset the form and the div after it********///////////
-function resetForm() {
-  document.getElementById('appointmentForm').innerHTML = '';
-  document.getElementById('appointform').reset();
-}
