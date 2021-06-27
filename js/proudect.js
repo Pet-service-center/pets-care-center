1f0d914cd7b4063bc3e7b2e14e5d9c804aa6cc2d
anas-abusaif <anasabusief@gmail.com>
2021-06-27 17:47:09 +0300

    product update

--- js/proudect.js
+++ js/proudect.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 const foodImg = [
   ['Keytee Parrarrot food', './images/food/birdsfood.jpg', '15$'],
   ['Whiskas Cat Food', './images/food/catfood.jpg', '12$'],
@@ -13,24 +12,30 @@ const foodImg = [
   [' food Dish', './images/food/foodDishes.jpg', '15$'],
   ['hamster food', './images/food/hamsterFood.jpg', '17$'],
   ['cat dish food ', './images/food/images.jpg', '40$'],
-  ['Menu hamster food', './images/food/hamsterfoods1.jpg', '12$']
+  ['Menu hamster food', './images/food/hamsterfoods1.jpg', '12$'],
 ];
-
-
 
 const clothesImg = [
   ['Cute cat pajama', './images/clothes/catpajama.jpg', '20$'],
   ['rabbit dress', './images/clothes/rabbitdress.jpg', '15$'],
   ['catWoolDress', './images/clothes/catWoolDress.jpg', '30$'],
   ['costumes-hamsters', './images/clothes/costumes-hamsters.jpg', '25$'],
-  ['dog-clothes-waterproof', './images/clothes/dog-clothes-waterproof.jpg', '18$'],
+  [
+    'dog-clothes-waterproof',
+    './images/clothes/dog-clothes-waterproof.jpg',
+    '18$',
+  ],
   ['dog pajamas', './images/clothes/dogpajamas.jpg', '27$'],
   ['hamster pajama', './images/clothes/hamsterpajama.jpg', '25$'],
   ['Dog Dress', './images/clothes/DogDress.jpg', '18$'],
-  [' Heartgown Dog Clothes Soft', './images/clothes/Heartgown Dog Clothes Soft.jpg', '35$'],
+  [
+    ' Heartgown Dog Clothes Soft',
+    './images/clothes/Heartgown Dog Clothes Soft.jpg',
+    '35$',
+  ],
   ['Parrot Suit Blazers', './images/clothes/Parrot-Suit-Blazers.jpg', '19$'],
   ['parrot dress', './images/clothes/parrot.jpg', '40$'],
-  ['parrot Dress', './images/clothes/parrotDress.jpg', '18$']
+  ['parrot Dress', './images/clothes/parrotDress.jpg', '18$'],
 ];
 
 const houseImg = [
@@ -45,7 +50,7 @@ const houseImg = [
   [' Dog outside house', './images/house/dogoutsidehouse.jpg', '135$'],
   ['rabbit house', './images/house/rabbithouse.jpg', '19$'],
   ['Wood bird house', './images/house/woodbirdhouse.jpg', '40$'],
-  ['Dog wood house', './images/house/dogwoodhouse.jpg', '78$']
+  ['Dog wood house', './images/house/dogwoodhouse.jpg', '78$'],
 ];
 const toolImg = [
   ['cat-toilet', './images/cat-toilet.jpg', '60$'],
@@ -55,11 +60,15 @@ const toolImg = [
   ['Dog shower tools', './images/dogshowertools.jpg', '18$'],
   ['Dog toys', './images/dogtoys.jpg', '27$'],
   ['Dog travel bag', './images/dogtravelbag.jpg', '25$'],
-  ['Rabbit & hamster travel bag', './images/Rabbit&hamsterTravelBag.jpg', '30$'],
+  [
+    'Rabbit & hamster travel bag',
+    './images/Rabbit&hamsterTravelBag.jpg',
+    '30$',
+  ],
   [' Shower tools', './images/Shower-Dog.jpg', '135$'],
   ['pets staris toy', './images/petstairs.jpg', '19$'],
   ['bag for small dog', './images/dogbag.jpg', '40$'],
-  ['Dog toys', './images/s.jpg', '78$']
+  ['Dog toys', './images/s.jpg', '78$'],
 ];
 const Product = function (name, src, price) {
   this.name = name;
@@ -67,9 +76,8 @@ const Product = function (name, src, price) {
   this.price = price;
   this.quantity = 0;
   Product.all.push(this);
-}
+};
 Product.all = [];
-
 
 function generate(array) {
   Product.all = [];
@@ -77,49 +85,36 @@ function generate(array) {
   for (let i = 0; i < array.length; i++) {
     new Product(array[i][0], array[i][1], array[i][2]);
   }
-
-
 }
-
-
 
 let section = document.getElementById('ProductsImages');
 
 function render() {
   for (let i = 0; i < Product.all.length; i++) {
     let div = document.createElement('div');
+    div.setAttribute('id',`no.${i}`);
     section.appendChild(div);
+    let infoPage= document.createElement('a');
+    infoPage.setAttribute('href','./productInfo.html');
+    div.appendChild(infoPage);
     let img = document.createElement('img');
     img.src = Product.all[i].src;
-    div.appendChild(img);
+    infoPage.appendChild(img);
     let h3 = document.createElement('h3');
     h3.textContent = Product.all[i].name;
     div.appendChild(h3);
-    
-    let input = document.createElement("INPUT");
-    input.setAttribute("type", "number");
-    input.setAttribute("placeholder", "Quantity");
-    input.setAttribute('min',0);
+    let input = document.createElement('INPUT');
+    input.setAttribute('type', 'number');
+    input.setAttribute('placeholder', 'Quantity');
+    input.setAttribute('min', 0);
     div.appendChild(input);
-    var btn = document.createElement("BUTTON");   // Create a <button> element
+    let btn = document.createElement('BUTTON');
     div.appendChild(btn);
     btn.innerHTML = 'Add to Cart';
   }
-
-  
-
-  
-
-
-};
-
-
-
-generate(foodImg);
-
+}
 function viewFood() {
   generate(foodImg);
-  //console.log(Product.all);
   render();
 }
 
@@ -127,13 +122,11 @@ generate(clothesImg);
 
 function viewclothes() {
   generate(clothesImg);
-  //console.log(Product.all);
   render();
 }
 
 function viewhouse() {
   generate(houseImg);
-  //console.log(Product.all);
   render();
 }
 
@@ -141,6 +134,7 @@ generate(toolImg);
 
 function viewtools() {
   generate(toolImg);
-  //console.log(Product.all);
   render();
-};
+}
+
+viewFood();
