5b634c2fd9e7766071397e89ea0e822eab57f604
anas-abusaif <anasabusief@gmail.com>
2021-06-27 16:04:16 +0300

    design update

--- js/home.js
+++ js/home.js
@@ -8,7 +8,7 @@ window.onscroll = function() {
 };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.documentElement.scrollTop > 20) {
     mybutton.style.display = 'block';
   } else {
     mybutton.style.display = 'none';
