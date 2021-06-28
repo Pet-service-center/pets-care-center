7f1ee2e72abbff810f3d7362d4489e0e0ea4e195
Suzan <suze-hiary@hotmail.com>
2021-06-28 12:06:28 +0300

    added css

--- js/proudect.js
+++ js/proudect.js
@@ -1,20 +1,21 @@
 'use strict';
 
 const foodImg = [
-  ['Keytee Parrarrot food', './images/food/birdsfood.jpg', '15$', 'Clean, high quality, wholesome seeds and grains with all the natural protein, fiber, oils, and nutrients of whole grains'],
-  ['Whiskas Cat Food', './images/food/catfood.jpg', '12$','Dry Cat Food, Ocean Fish flavor with milk This is a Non-vegetarian product.Made with high quality ingredients.No artificial flavourings or preservatives.A perfect balance of vitamins and minerals.'],
-  ['pedigree Dog Food', './images/food/download.jpg', '30$',' Adult Complete Nutrition Roasted Chicken, Rice & Vegetable Flavor Dry Dog Food is formulated to give dogs all of the energy and nourishment .This chicken-flavor dog food is packed with B vitamins, zinc and omega-6 fatty acid .contains leading levels of the antioxidant, vitamin E. '],
-  ['Food-And-Water-Dispenser', './images/food/allBirdsfood.jpg', '25$','White Millet, Oats, Sun-Cured Timothy Hay, Wheat, Sun-Cured Alfalfa Hay, Corn, Sunflower Seeds, Red Milo, Flaked Peas, Flaked Beans, Peanuts, Yellow Peas, Flaked Carrots, Flaked Corn, Soybean Hulls, Dried Beet Pulp (sugar removed).'],
-  ['Wild Bird Food', 'images/food/WildBirdFood.jpg', '18$','Attracts a wide variety of birds Seed mix is perfect for hopper feeders, platform feeders and tube feedersContains millet, black oil sunflower seed, cracked corn, wheat and vitaminsMade in the USA'],
-  ['rabbit food', './images/food/rabbitfoods.jpg', '24$','high-quality mix that contains all of the natural protein, fiber, oils and nutrients of whole grains. Fortified to ensure a balance blend, Supreme utilizes natural seeds, grains and pellets to provide your small pet with a simple yet healthy diet.'],
-  ['water continer', './images/food/waterdishes.jpg', '25$','Clear High Quality Accrylic Plastic finish-Dishwasher safe. Design blends in with any decor.20 year patented proven design - Hand made in U.S.A.Two Height adjustments 7" and 9" off the ground. Fits most small to medium sized dogs.'],
-  ['Rabbit Food', './images/food/rabbitfood.jpg', '25$','A healthy, all-natural blend of species-appropriate dried mixed fruits, seeds, grains, nuts and pre-cooked legumes.Enhanced with vitamins, minerals and amino acids.Added DHA omega-3 and -6 fatty acids contribute valuable antioxidants.Protected probiotics deliver beneficial gut bacteria.Includes no artificial colors, flavors or preservatives.'],
+  ['Keytee Parrarrot food', './images/food/birdsfood.jpg', '15$', 'Clean, high quality, wholesome seeds and grains with all the natural protein,fiber, oils, and nutrients of whole grains.'],
+  ['Whiskas Cat Food', './images/food/catfood.jpg', '12$','Dry Cat Food, Ocean Fish flavor with milk This is a Non-vegetarian product.Made with high quality ingredients.No artificial flavourings.'],
+  ['pedigree Dog Food', './images/food/download.jpg', '30$',' Adult Complete Nutrition Roasted Chicken, Rice & Vegetable Flavor Dry Dog Food is formulated to give dogs all of the energy and nourishment. '],
+  ['Food-And-Water-Dispenser', './images/food/allBirdsfood.jpg', '25$','White Millet, Oats, Sun-Cured Timothy Hay, Wheat, Sun-Cured Alfalfa Hay, Corn, Sunflower Seeds, Red Milo, Flaked Peas, Flaked Beans.'],
+  ['Wild Bird Food', 'images/food/WildBirdFood.jpg', '18$','Attracts a wide variety of birds Seed mix is perfect for hopper feeders, platform feeders and tube feedersContains millet, black oil sunflower seed, cracked corn, wheat and vitaminsMade in the USA.'],
+  ['rabbit food', './images/food/rabbitfoods.jpg', '24$','high-quality mix that contains all of the natural protein, fiber, oils and nutrients of whole grains. Fortified to ensure a balance blend, Supreme utilizes natural seeds. '],
+  ['water continer', './images/food/waterdishes.jpg', '25$','Clear High Quality Accrylic Plastic finish-Dishwasher safe. Design blends in with any decor.20 year patented proven design - Hand made in U.S.A. '],
+  ['Rabbit Food', './images/food/rabbitfood.jpg', '25$','A healthy, all-natural blend of species-appropriate dried mixed fruits, seeds, grains, nuts and pre-cooked legumes.Enhanced with vitamins, minerals and amino acids.'],
 
-  [' food Dish', './images/food/foodDishes.jpg', '15$','Non-Skidding & Flipping - The bone-shaped base is made of food-grade silicone, and it is designed to better hold the bowls and prevent them from tipping and skidding when your dog is eating.No-Spill Silicone Mat- The raised silicone mat edge can catch most spills and splashes and keep your floor clean, and also save you much pet food and time'],
-  ['hamster food', './images/food/hamsterFood.jpg', '17$','White Millet, Oats, Sun-Cured Timothy Hay, Wheat, Sun-Cured Alfalfa Hay, Corn, Sunflower Seeds, Red Milo, Flaked Peas, Flaked Beans, Peanuts, Yellow Peas, Flaked Carrots, Flaked Corn, Soybean Hulls, Dried Beet Pulp (sugar removed).'],
-  ['cat dish food ', './images/food/images.jpg', '40$','Cat Food Bowls Set,Raised Cat Bowls for Food and Water,Ceramic Elevated Pet Dishes Bowls with Stand,12 oz Cats and Small Dogs Bowls,Dishwasher Safe'],
-  ['Menu hamster food', './images/food/hamsterfoods1.jpg', '12$','Contains essentials vitamins, minerals & trace elementsGood quality extruded productsLinoleic acid and amino acids added Sugar less recipe, no artificial flavoring'],
+  [' food Dish', './images/food/foodDishes.jpg', '15$','Non-Skidding & Flipping - The bone-shaped base is made of food-grade silicone, and it is designed to better hold the bowls and prevent them from tipping and skidding. '],
+  ['hamster food', './images/food/hamsterFood.jpg', '17$','White Millet, Oats, Sun-Cured Timothy Hay, Wheat, Sun-Cured Alfalfa Hay, Corn, Sunflower Seeds, Red Milo, Flaked Peas, Flaked Beans, Peanuts, Yellow Peas, Flaked Carrots. '],
+  ['cat dish food ', './images/food/images.jpg', '40$','Cat Food Bowls Set,Raised Cat Bowls for Food and Water,Ceramic Elevated Pet Dishes Bowls with Stand,12 oz Cats and Small Dogs Bowls,Dishwasher Safe.'],
+  ['Menu hamster food', './images/food/hamsterfoods1.jpg', '12$','Contains essentials vitamins, minerals & trace elementsGood quality extruded productsLinoleic acid and amino acids added Sugar less recipe, no artificial flavoring.'],
 ];
+
 
 const clothesImg = [
   ['Cute cat pajama', './images/clothes/catpajama.jpg', '20$','Adorable cute coton cat pajama'],
@@ -58,7 +59,7 @@ const toolImg = [
   ['pets staris toy', './images/petstairs.jpg', '19$','KONG AirDog combines two classic dog toys -the tennis ball texture and the squeaker-to create the perfect fetch toy. Now available in dual materials for added strength, the non-abrasive, durable and high-quality material will not wear down dogs teeth.'],
   ['bag for small dog', './images/dogbag.jpg', '40$','Pet Carrier Backpack Adjustable Pet Front Cat Dog Carrier Travel Bag Legs Out'],
   ['Dog toys', './images/s.jpg', '78$','KONG AirDog combines two classic dog toys -the tennis ball texture and the squeaker-to create the perfect fetch toy. Now available in dual materials for added strength, the non-abrasive, durable and high-quality material will not wear down dogs teeth.'],
-  ['bag for small dog', './images/dogbag.jpg', '40$','Pet Carrier Backpack Adjustable Pet Front Cat Dog Carrier Travel Bag Legs Out'],
+ 
 ];
 
 
@@ -134,33 +135,35 @@ function render() {
     h3.textContent = Product.all[i].name;
     div.appendChild(h3);
 
+    
+    
     let p = document.createElement('p');
     div.appendChild(p);
     p.textContent = Product.all[i].description;
     p.classname="description";
      
-    let br = document.createElement('br');
-    p.appendChild(br);
-    br.innerHTML='';
     
     let input = document.createElement('INPUT');
     input.setAttribute('type', 'number');
     input.setAttribute('placeholder', 'Quantity');
     input.setAttribute('min', 0);
     div.appendChild(input);
-    input.className="input";
+    
+
+
+
     let price = document.createElement('h3')
     div.appendChild(price);
     price.textContent = `price  ${Product.all[i].price}  `;
     price.id="price";
 
     
-
+    
 
     let btn = document.createElement('BUTTON');
     div.appendChild(btn);
     btn.innerHTML = 'Add to Cart';
-
+    btn.className="input";
     btn.setAttribute('id', Product.all[i].id);
    
  }
